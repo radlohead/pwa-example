f6c961e0226e8f0dcee8ccce927ecca521e8ba1f
= <radlohead@naver.com>
2019-08-04 21:00:02 +0900

    desktop application install

--- js/index.js
+++ js/index.js
@@ -3,3 +3,30 @@ if ('serviceWorker' in navigator) {
         console.log('Service Worker Registered')
     })
 }
+
+const addBtn = document.querySelector('.add-button')
+addBtn.style.display = 'none'
+
+let deferredPrompt
+window.addEventListener('beforeinstallprompt', e => {
+    e.preventDefault()
+    deferredPrompt = e
+    addBtn.style.display = 'block'
+
+    addBtn.addEventListener('click', e => {
+        addBtn.style.display = 'none'
+        deferredPrompt.prompt()
+        deferredPrompt.userChoice.then(choiceResult => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt')
+            } else {
+                console.log('User dismissed the A2HS prompt')
+            }
+            deferredPrompt = null
+        })
+    })
+})
+
+window.addEventListener('appinstalled', () => {
+    console.log('app installed')
+})
